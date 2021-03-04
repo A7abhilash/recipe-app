6933c1617478b184c66d894b85107eac796983aa
A7abhilash <a7abhilash@gmail.com>
2021-03-04 21:28:50 +0530

    added search bar

---
+++ src/components/Search.js
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+import { StyleSheet, View, TouchableOpacity, Alert } from "react-native";
+import { FontAwesome } from "@expo/vector-icons";
+import { TextInput } from "react-native-paper";
+
+export default function Search({ searchQuery }) {
+  const [text, setText] = useState("");
+
+  const handlePress = () => {
+    if (!text) {
+      Alert.alert("Error", "Please enter something", [
+        {
+          text: "Understood",
+        },
+      ]);
+    } else {
+      searchQuery(text);
+    }
+  };
+
+  return (
+    <View style={styles.inputContainer}>
+      <TextInput
+        // placeholder="Search something..."
+        label="Search something..."
+        onChangeText={(val) => setText(val)}
+        value={text}
+        style={styles.input}
+      />
+      <TouchableOpacity style={styles.button} onPress={handlePress}>
+        <FontAwesome name="plus" size={25} color="#eee" />
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    margin: 10,
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  input: {
+    fontSize: 18,
+    flex: 7,
+  },
+  button: {
+    backgroundColor: "darkslateblue",
+    flex: 1,
+    marginLeft: 20,
+    justifyContent: "center",
+    alignItems: "center",
+    height: 60,
+    padding: 10,
+    borderRadius: 8,
+  },
+});
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,16 +1,33 @@
-import React from "react";
-import { Button, StyleSheet, Text, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import { StyleSheet, View } from "react-native";
+import Search from "../components/Search";
+import { APP_ID, APP_KEY } from "./../keys";
 
 export default function Home({ navigation }) {
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+  const [query, setQuery] = useState("pizza");
+  const [list, setList] = useState([]);
+
+  useEffect(() => {
+    fetch(
+      `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      });
+  }, [query]);
+
   return (
-    <View>
-      <Text>Home</Text>
-      <Button
-        title="Go Somewhere"
-        onPress={() => navigation.navigate("Complete Recipe")}
-      />
+    <View style={styles.container}>
+      <Search searchQuery={setQuery} />
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    margin: 10,
+  },
+});
